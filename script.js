f626ab147859f4af37e70b438224a313545ab2bd
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-17 15:23:57 +0100

    MILESTONE 1 - completata + aggiunto cartella img

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let membriTeam = [
 
-    {nome:"Wayne Barnett",ruolo:"Founder & CEO",foto:"wayne-barnett-founder-ceo.jpg"},
-    {nome:"Angela Caroll",ruolo:"Chief Editor",foto:"angela-caroll-chief-editor.jpg"},
-    {nome:"Walter Gordon",ruolo:"Office Manager",foto:"walter-gordon-office-manager.jpg"},
-    {nome:"Angela Lopez",ruolo:"Social Media Manager",foto:"angela-lopez-social-media-manager.jpg"},
-    {nome:"Scott Estrada",ruolo:"Developer",foto:"scott-estrada-developer.jpg"},
-    {nome:"Barbara Ramos",ruolo:"Graphic Designer",foto:"barbara-ramos-graphic-designer.jpg"}
+    {nome:"Wayne Barnett",ruolo:"Founder & CEO",foto:"./img/wayne-barnett-founder-ceo.jpg"},
+    {nome:"Angela Caroll",ruolo:"Chief Editor",foto:"./img/angela-caroll-chief-editor.jpg"},
+    {nome:"Walter Gordon",ruolo:"Office Manager",foto:"./img/walter-gordon-office-manager.jpg"},
+    {nome:"Angela Lopez",ruolo:"Social Media Manager",foto:"./img/angela-lopez-social-media-manager.jpg"},
+    {nome:"Scott Estrada",ruolo:"Developer",foto:"./img/scott-estrada-developer.jpg"},
+    {nome:"Barbara Ramos",ruolo:"Graphic Designer",foto:"./img/barbara-ramos-graphic-designer.jpg"}
 
 ]
+
+membriTeam.forEach((element) => {
+    console.log(`
+        ================= Membro ==================
+        
+        Nome: ${element.nome}
+        Ruolo: ${element.ruolo}
+        Foto: ${element.foto}
+
+        ===========================================
+    `)
+});
